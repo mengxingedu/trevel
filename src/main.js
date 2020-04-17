f4ae634f102b0417ce0bdf1e1f1d2681b0874b01
梦醒 <2633144178@qq.com>
2020-04-17 17:00:19 +0800

    新增项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
+import Vue from 'vue'  
 import App from './App.vue'
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true;  //对于开发版本，会默认向控制台打印：设置为false就不会提示了
+fastClick.attach(document.body)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../pages/home/home'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    // name: 'Home',
+    component: Home
+  },
+  {
+    path: '/list',
+    // name: 'list',
+    component: () => import(/* webpackChunkName: "about" */ '../pages/list/list')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
