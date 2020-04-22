feaeb00de6bdfc6c76ace56bc826c195f5950037
谢志鹏 <xiezhipeng@tvdcy.net>
2020-04-23 00:23:13 +0800

    新增轮播图

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <home-header></home-header>
+        <home-swiper></home-swiper>
     </div>
 </template>
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper';
 export default{
     name : 'Home',
     components: {
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
     }
 }
 </script>
