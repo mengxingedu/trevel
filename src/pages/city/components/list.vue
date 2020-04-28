a9bfa62505650b0d22a88552e05ec459710e4633
谢志鹏 <xiezhipeng@tvdcy.net>
2020-04-28 23:39:12 +0800

    位置功能完善

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list">
+    <div class="list" ref='wrapper'>
         <div>
             <div class="area">
                 <div class="title border ">你的位置</div>
@@ -71,48 +71,52 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default{
-    name : 'CityList'
+    name : 'CityList',
+    mounted (){
+        this.scroll = new BScroll(this.$refs.wrapper)
+    }
 }
 </script>
 
-<style lang='stylus' scoped>
+<style lang='stylus' scoped> 
 @import '~@/assets/styles/varibles.styl'
 @import '~@/assets/styles/mxmins.styl'
 
-.list
-    width 100%
-    height 100%
-    position absolute
-    top : 1.58rem
-    left: 0
-    overflow hidden
-    .title
-        font-size .26rem
-        line-height .5rem
-        height .5rem
-        padding-left .2rem
-        background  #eee
-        border-bottom: .01rem solid #48484863
-        border-top: .01rem solid #48484863
-    .areaList
-        padding .2rem .2rem 0
+    .list
+        position absolute
+        top : 1.58rem
+        left: 0
+        right 0
+        bottom 0
         overflow hidden
-        .dizi
-            padding .2rem
-            border .02rem solid #eee
-            text-align center
-            float left
-            width calc(94% / 3 - .4rem)
-            margin-left 2%
-            margin-bottom .2rem
-        .dizi:nth-child(3n - 2)
-            margin-left 0%
-    .zmlist
-        line-height .7rem
-        height .7rem
-        padding-left .2rem
-        border-bottom  .01rem solid #48484863 
-    .zmlist:last-child
-        border none
+        .title
+            font-size .26rem
+            line-height .5rem
+            height .5rem
+            padding-left .2rem
+            background  #eee
+            border-bottom: .01rem solid #48484863
+            border-top: .01rem solid #48484863
+        .areaList
+            padding .2rem .2rem 0
+            overflow hidden
+            .dizi
+                padding .2rem
+                border .02rem solid #eee
+                text-align center
+                float left
+                width calc(94% / 3 - .4rem)
+                margin-left 2%
+                margin-bottom .2rem
+            .dizi:nth-child(3n - 2)
+                margin-left 0%
+        .zmlist
+            line-height .7rem
+            height .7rem
+            padding-left .2rem
+            border-bottom  .01rem solid #48484863 
+        .zmlist:last-child
+            border none
 </style>
