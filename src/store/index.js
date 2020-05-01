db9fd1ddd4cad5e65769303e626bbf27b07cff6c
mengxingedu <2633144178@qq.com>
2020-05-01 21:02:33 +0800

    beijin

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex) //vue里使用插件需要use
 
 export default new Vuex.Store({
     state : {
-        cities : '上海'
+        cities : '北京'
       },
       mutations : {
         increment (states, str){
